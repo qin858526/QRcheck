8cd4aea7934df080e69b26b2a22dbc0959b78d19
qin858526 <453910959@qq.com>
2026-03-30 15:52:11 +0800

    flask开启debug模式

--- app.py
+++ app.py
@@ -146,5 +146,5 @@ def logout():
     return redirect(url_for('login_page'))
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8888)
-    # app.run(debug=True, host='0.0.0.0', port=8888)
+    # serve(app, host='0.0.0.0', port=8888)
+    app.run(debug=True, host='0.0.0.0', port=8888)
